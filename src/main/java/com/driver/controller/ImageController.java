3b4ff2f4c3d7f5efb11bc782f9f6769b5f5d5ed5
Kiranchandu Saragadam <110031953+kiranchandusaragadam@users.noreply.github.com>
2023-11-18 12:27:51 +0530

    7 update

--- src/main/java/com/driver/controller/ImageController.java
+++ src/main/java/com/driver/controller/ImageController.java
@@ -16,10 +16,10 @@ public class ImageController {
     ImageService imageService;
 
     @PostMapping("/{blogId}/add-image")
-    public ResponseEntity<Image> addImage(@PathVariable int blogId, @RequestParam String description, @RequestParam String dimensions) {
+    public ResponseEntity<String> addImage(@PathVariable int blogId, @RequestParam String description, @RequestParam String dimensions) {
         // Add image into the give blog
-        Image image = imageService.addImage(blogId, description, dimensions);
-        return new ResponseEntity<>(image, HttpStatus.OK);
+        imageService.addImage(blogId, description, dimensions);
+        return new ResponseEntity<>("Added image successfully", HttpStatus.CREATED);
     }
 
     @GetMapping("/countImagesInScreen/{id}/{screenDimensions}")
